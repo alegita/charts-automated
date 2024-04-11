8a51e6ea2199823dd45b7ed442b86c5152fb35b8
alegita <69376514+alegita@users.noreply.github.com>
2024-04-11 15:17:44 -0400

    add utils, get population functions

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
-
+import read_csv
+import charts
 
 def run():
     data = read_csv.read_csv('data.csv')
-    data = list(filter(lambda item: item))
+    data = list(filter(lambda item: item['Continent'] == 'South America', data))
+    
+    countries = list(map(lambda x:x['Country'], data))
+    percentages = list(map(lambda x:x['World Population Percentage'], data))
+    charts.generate_pie_chart(countries, percentages)
